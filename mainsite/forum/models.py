eeed6d1db5177d9bd1e52174385f8971fc3538c6
Riku Putkinen <putkinenriku@gmail.com>
2023-11-07 19:13:52 +0200

    Initial commit

---
+++ mainsite/forum/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Board(models.Model):
+    boad_name = models.CharField(max_length=50)
+
+class Post(models.Model):
+    post_text = models.TextField()
+    post_date = models.DateTimeField("Date posted")
+    board_id = models.ForeignKey(Board, on_delete=models.CASCADE)
+
+class Reply(models.Model):
+    nickname = models.CharField(max_length=20)
+    reply_text = models.TextField()
+    post_date = models.DateTimeField("Date posted")
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
