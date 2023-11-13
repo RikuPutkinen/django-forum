6693c4938493c04884a2f8227f463bbe2246df1d
Riku Putkinen <putkinenriku@gmail.com>
2023-11-13 19:47:04 +0200

    Fix model

--- mainsite/forum/models.py
+++ mainsite/forum/models.py
@@ -4,6 +4,8 @@ class Board(models.Model):
     boad_name = models.CharField(max_length=50)
 
 class Post(models.Model):
+    nickname = models.CharField(max_length=20)
+    post_title = models.CharField(max_length=50, default="")
     post_text = models.TextField()
     post_date = models.DateTimeField("Date posted")
     board_id = models.ForeignKey(Board, on_delete=models.CASCADE)
