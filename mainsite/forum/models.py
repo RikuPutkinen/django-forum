8aee9319966e22c6f697f549b8b1e07d4a326cc8
Riku Putkinen <putkinenriku@gmail.com>
2023-11-21 03:14:24 +0200

    Add __str__ methods to models

--- mainsite/forum/models.py
+++ mainsite/forum/models.py
@@ -3,6 +3,10 @@ from django.db import models
 class Board(models.Model):
     board_name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.board_name
+
+
 class Post(models.Model):
     nickname = models.CharField(max_length=20)
     post_title = models.CharField(max_length=50, default="")
@@ -10,8 +14,15 @@ class Post(models.Model):
     post_date = models.DateTimeField("Date posted")
     board_id = models.ForeignKey(Board, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.post_title
+
+
 class Reply(models.Model):
     nickname = models.CharField(max_length=20)
     reply_text = models.TextField()
     post_date = models.DateTimeField("Date posted")
     post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.reply_text[:50]
